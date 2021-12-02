3d6c6c9b9af8b30fa1b59e0b00f2e75bebc08167
AntonBoreiko <1213.96@mail.ru>
2021-12-02 19:52:56 +0300

    Fix project/ add createEntityAdapter

--- src/components/heroesFilters/HeroesFilters.js
+++ src/components/heroesFilters/HeroesFilters.js
@@ -2,19 +2,20 @@ import { useHttp } from '../../hooks/http.hook';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import classNames from 'classnames';
+import store from '../../store'
 
-import { fetchFilters } from './filtersSlice';
-import { filtersChanged } from './filtersSlice'
+import { fetchFilters, filtersChanged, selectAll } from './filtersSlice';
 import Spinner from '../spinner/Spinner';
 
 const HeroesFilters = () => {
 
-    const { filters, filtersLoadingStatus, activeFilter } = useSelector(state => state.filters);
+    const { filtersLoadingStatus, activeFilter } = useSelector(state => state.filters);
+    const filters  = selectAll(store.getState())
     const dispatch = useDispatch();
     const { request } = useHttp();
 
     useEffect(() => {
-        dispatch(fetchFilters());
+        dispatch(fetchFilters(request));
         //   eslint-disable-next-line
     }, []);
 
