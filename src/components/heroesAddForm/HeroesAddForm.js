e950f548f46eb6092a2abb9c7d913a8bd483342e
AntonBoreiko <1213.96@mail.ru>
2021-12-06 23:37:44 +0300

    Add RTK Query in project

--- src/components/heroesAddForm/HeroesAddForm.js
+++ src/components/heroesAddForm/HeroesAddForm.js
@@ -6,17 +6,19 @@ import { v4 as uuidv4 } from 'uuid';
 import store from '../../store'
 
 import { selectAll } from '../heroesFilters/filtersSlice';
-import { heroCreated } from '../heroesList/heroesSlice';
+import { useCreateHeroMutation } from '../../api/apiSlice';
 
 const HeroesAddForm = () => {
+
+
     const [heroName, setHeroName] = useState('');
     const [heroDescr, setHeroDescr] = useState('');
     const [heroElement, setHeroElement] = useState('');
 
+    const [createHero, { isLoading }] = useCreateHeroMutation()
+
     const { filtersLoadingStatus } = useSelector(state => state.filters);
     const filters = selectAll(store.getState())
-    const dispatch = useDispatch();
-    const { request } = useHttp();
 
     const onSubmitHandler = (e) => {
         e.preventDefault();
@@ -28,9 +30,7 @@ const HeroesAddForm = () => {
             element: heroElement
         }
 
-        request("http://localhost:3001/heroes", "POST", JSON.stringify(newHero))
-            .then(dispatch(heroCreated(newHero)))
-            .catch(err => console.log(err));
+        createHero(newHero).unwrap()
 
         setHeroName('');
         setHeroDescr('');
